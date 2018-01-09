4960d1d6e95e50d98c3d197bbbadb964f5387362
Gregor Gebhardt <gebhardt@ias.tu-darmstadt.de>
2018-01-09 13:54:18 +0100

    added SimplePhototaxisKilobot to __init__.py

--- gym_kilobots/lib/__init__.py
+++ gym_kilobots/lib/__init__.py
@@ -1,3 +1,3 @@
-from .kilobot import Kilobot, PhototaxisKilobot
+from .kilobot import Kilobot, PhototaxisKilobot, SimplePhototaxisKilobot
 from .body import Quad, CornerQuad, Circle, CForm, TForm, LForm
 from .light import CircularGradientLight, GradientLight
