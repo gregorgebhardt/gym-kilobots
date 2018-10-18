dc98811191c3d86f9c6c441abfe58beaf4a774eb
Gregor Gebhardt <gregor.gebhardt@gmail.com>
2018-10-18 23:11:49 +0200

    removed some environments

--- gym_kilobots/envs/__init__.py
+++ gym_kilobots/envs/__init__.py
@@ -1,5 +1,2 @@
 from .kilobots_env import KilobotsEnv
-
-from .kilobots_empty_envs import UniteKilobotsEnv
-
-from .kilobots_quad_envs import QuadAssemblyKilobotsEnv, GradientLightEnv
+from .yaml_kilobots_env import YamlKilobotsEnv
